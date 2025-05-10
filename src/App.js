f26dd065681c3ad5ecf7423535a8e9ad4c24ec78
sakshi pandya <sakshipandya220411@acropolis.in>
2025-05-10 22:06:21 +0530

    card feature'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './screens/Home';
+import Login from  './screens/Login';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+       <div className="App">
+     <Routes>
+      <Route exact path="/" element={<Home/>} >
+        
+      </Route>
+      <Route exact path="/Login" element={<Login/>} >
+        
+      </Route>
+     </Routes>
     </div>
+    </Router>
+   
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Card from '../components/Card'
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Card/>
+      <Footer/>
+    </div>
+  )
+}
