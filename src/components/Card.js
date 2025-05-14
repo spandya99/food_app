a6e432d39ffa522846994edb9f8b3d5aed7c3055
sakshi pandya <sakshipandya79@gmail.com>
2025-05-15 00:37:07 +0530

    some changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,12 @@ export default function Card() {
   return (
     <>
       <div className="card mt-5" style={{"width": "18rem"}}>
-      <img src="..." className="card-img-top" alt="..."></img>
+      <img src="https://www.cookwithmanali.com/wp-content/uploads/2015/07/Restaurant-Style-Recipe-Paneer-Tikka-500x500.jpg" className="card-img-top " alt="..."></img>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 <div className='container'>
-<select>
+<select className='m-2 h-100 bg-success rounded '>
     {
         Array.from(Array(6),(e,i)=>(
            <option key={i+1} value={i+1}>{i+1}</option>
@@ -18,7 +18,7 @@ export default function Card() {
         )    
     }
 </select>
-<select>
+<select className='m-2 h-100 bg-success rounded '>
    <option >half</option>
     <option >full</option>
 </select>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar/>
-      <Card/>
+      <Carousel/>
+      <div className='m-2'><Card/></div>
+      
       <Footer/>
-    </div>
+    </>
   )
 }
